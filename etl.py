2da3c28fba8dc020b7b2f5a9df87e49c7f750dca
Haran Arbel <haranarbel@gmail.com>
2022-01-19 14:22:48 +0200

    added times table

--- etl.py
+++ etl.py
@@ -3,7 +3,7 @@ import glob
 import psycopg2
 import pandas as pd
 from sql_queries import *
-
+from datetime import datetime
 
 def process_song_file(cur, filepath):
     # open song file
@@ -20,18 +20,19 @@ def process_song_file(cur, filepath):
 
 def process_log_file(cur, filepath):
     # open log file
-    df = 
+    df = pd.read_json(filepath, lines=True)
 
     # filter by NextSong action
-    df = 
+    df = df[df["page"] == "NextSong"]
 
     # convert timestamp column to datetime
-    t = 
+    t = pd.to_datetime(df.ts, unit = 'ms')
     
     # insert time data records
-    time_data = 
-    column_labels = 
-    time_df = 
+    time_data = (t, t.dt.hour, t.dt.day, t.dt.isocalendar().week, t.dt.month, t.dt.year, t.dt.weekday)
+    column_labels = ('start_time', 'hour', 'day', 'week', 'month', 'year', 'weekday')
+    time_df = pd.DataFrame(dict(zip(column_labels, time_data)))
+
 
     for i, row in time_df.iterrows():
         cur.execute(time_table_insert, list(row))
